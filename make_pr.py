e0ad75027f864e5b4c49aa7836ca3c87193425f2
Caroline Landry <caroline.landry@inria.fr>
2018-05-29 15:45:43 +0200

    mardi 29 mai 2018, 15:45:43 (UTC+0200)

--- make_pr.py
+++ make_pr.py
@@ -2,7 +2,8 @@
 
 from github3 import login
 
-myToken="db9c0107e3cbc59effd12d063f7e70d9b3d80ba0"
+# myToken="db9c0107e3cbc59effd12d063f7e70d9b3d80ba0"
+myToken="75026ee8330c1dae2a0d31e69dda1d5e78828d73"
 
 githubLogin = login(token=myToken)
 repo = githubLogin.repository('CaelProjects', 'experiments')
